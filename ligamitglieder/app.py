2213e69a4e1c108b960739092724800e6b6ebdf6
Marvin A. Ruder <ruder.marvin@icloud.com>
2020-11-02 22:52:07 +0100

    Bugfixes

--- ligamitglieder/app.py
+++ ligamitglieder/app.py
@@ -1,3 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
 from flask import Flask , request, render_template, session, flash, redirect, url_for, g, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager , UserMixin, login_user, login_required, logout_user, current_user
@@ -12,7 +15,7 @@ import json
 import copy
 import sqlconfig
 import urllib
-import send_mail
+import sendmail
 
 
 app = Flask(__name__)
@@ -243,7 +246,7 @@ def edit(user_id):
     return render_template('edit.html',user = user, geburtsdatum=geburtsdatum,\
     erstellungsdatum=erstellungsdatum)
     
-@app.route('/send_mail/<user_id>')
+@app.route('/send_mail/<user_id>', methods=['GET', 'POST'])
 @login_required
 def send_mail(user_id):
     user = kunden.query.filter_by(id=user_id).first()
@@ -260,56 +263,56 @@ def send_mail(user_id):
     subject = f"Herzlich willkommen bei den Liberalen Gamern, {user.vorname}!"
 
     text = f"""\
-    Hallo {user.vorname},<br />
-    <br />
-    ich bin Marvin von den Liberalen Gamern und möchte dich bei uns herzlich willkommen heißen!<br />
-    <br />
-    Wir haben dich mit den folgenden Daten in unser Mitgliederverzeichnis aufgenommen &ndash; schau einmal, ob alles richtig ist:<br />
-    <br />
-    Vorname: {user.vorname}<br />
-    Name: {user.name}<br />
-    Anschrift: {user.strasse}&nbsp;{user.hausnummer}<br />
-    PLZ, Ort: {user.plz}&nbsp;{user.ort}<br />
-    E-Mail-Adresse: {user.email}<br />
-    Handynummer: {user.mobil}<br />
-    Geburtsdatum: {geburtsdatum}<br />
-    <br />
-    Mitgliedsnummer: {user_id}<br />
-    <br />
-    Hier noch ein paar hilfreiche Links rund um die LiGa:<br />
-    <br />
-    Unsere offiziellen Social-Media-Kanäle (gerne teilen und liken):<br />
-    <a href="https://www.facebook.com/liberalegamer">https://www.facebook.com/liberalegamer</a><br />
-    <a href="https://twitter.com/LiberaleGamer">https://twitter.com/LiberaleGamer</a><br />
-    <a href="https://www.instagram.com/LiberaleGamer">https://www.instagram.com/LiberaleGamer</a><br />
-    <br />
-    Unsere verbandsinternen Dokumente wie Beschlüsse, Satzung oder Geschäftsordnung dokumentieren wir in unserem Wiki: <a href="https://wiki.liberale-gamer.gg">https://wiki.liberale-gamer.gg</a><br />
-    <br />
-    Unsere geschlossene Facebook-Gruppe: <a href="https://www.facebook.com/groups/433296140360035/">https://www.facebook.com/groups/433296140360035/</a><br />
-    Unserer geschlossenen WhatsApp-Gruppe kann ich dich hinzufügen, wenn du mir dafür auf WhatsApp eine kurze Nachricht schreibst &ndash; mit <a href="https://api.whatsapp.com/send?phone=4917657517450&text=Hallo+Marvin%2C+bitte+f%C3%BCge+mich+der+LiGa-WhatsApp-Gruppe+hinzu%21+{url_vorname}+{url_name}+%28Mitgliedsnummer+{user_id}%29%2E%20Ich+bin+damit+einverstanden%2C+dass+mit+Hinzuf%C3%BCgen+in+diese+Gruppe+meine+Handynummer+an+WhatsApp+und+alle+Mitglieder+in+der+Gruppe+weitergegeben+wird%2E">diesem Link</a> geht das ganz einfach. Dazu ein notwendiger Hinweis zum Datenschutz: Wenn du uns mitteilst, dass du zu unserer LiGa-WhatsApp-Gruppe hinzugefügt werden möchtest, erklärst du dich damit einverstanden, dass mit Hinzufügen in diese Gruppe deine Handynummer an WhatsApp und alle Mitglieder in der Gruppe weitergegeben wird.<br />
-    <br />
-    Noch ein kurzer Hinweis zu der WhatsApp-Gruppe: Diese ist ausschließlich für Vereins- und Gaming-Themen gedacht. Du findest in der Gruppenbeschreibung aber auch einen Link zur LiGa-Talk-Gruppe, in der über alles Mögliche diskutiert werden kann. Auch haben wir weitere Gruppen speziell für einzelne Spiele eingerichtet. Eine Liste mit Einladungslinks dazu findest du ebenfalls in der Gruppenbeschreibung.<br />
-    <br />
-    Falls du irgendwelche Fragen hast, kannst du dich jederzeit gerne an mich wenden.<br />
-    <br />
-    Liebe Grüße<br />
-    <br />
-    Marvin Ruder<br />
-    <br />
-    <br />
-    ————————————————————————————————<br />
-    <br />
-    Liberale Gamer e.V.<br />
-    <br />
-    Marvin Ruder<br />
-    Mitgliederbetreuung<br />
-    <br />
-    Tel.:&tab; 06224 9266995<br />
-    Fax:&tab; 06224 9282579<br />
-    Mobil:&tab;0176 57517450<br />
-    <br />
-    <a href="mailto:marvin.ruder@liberale-gamer.gg">marvin.ruder@liberale-gamer.gg</a><br />
-    <a href="https://www.liberale-gamer.gg">www.liberale-gamer.gg</a><br />"""
+Hallo {user.vorname},<br />
+<br />
+ich bin Marvin von den Liberalen Gamern und möchte dich bei uns herzlich willkommen heißen!<br />
+<br />
+Wir haben dich mit den folgenden Daten in unser Mitgliederverzeichnis aufgenommen &ndash; schau einmal, ob alles richtig ist:<br />
+<br />
+Vorname: {user.vorname}<br />
+Name: {user.name}<br />
+Anschrift: {user.strasse}&nbsp;{user.hausnummer}<br />
+PLZ, Ort: {user.plz}&nbsp;{user.ort}<br />
+E-Mail-Adresse: {user.email}<br />
+Handynummer: {user.mobil}<br />
+Geburtsdatum: {geburtsdatum}<br />
+<br />
+Mitgliedsnummer: {user_id}<br />
+<br />
+Hier noch ein paar hilfreiche Links rund um die LiGa:<br />
+<br />
+Unsere offiziellen Social-Media-Kanäle (gerne teilen und liken):<br />
+<a href="https://www.facebook.com/liberalegamer">https://www.facebook.com/liberalegamer</a><br />
+<a href="https://twitter.com/LiberaleGamer">https://twitter.com/LiberaleGamer</a><br />
+<a href="https://www.instagram.com/LiberaleGamer">https://www.instagram.com/LiberaleGamer</a><br />
+<br />
+Unsere verbandsinternen Dokumente wie Beschlüsse, Satzung oder Geschäftsordnung dokumentieren wir in unserem Wiki: <a href="https://wiki.liberale-gamer.gg">https://wiki.liberale-gamer.gg</a><br />
+<br />
+Unsere geschlossene Facebook-Gruppe: <a href="https://www.facebook.com/groups/433296140360035/">https://www.facebook.com/groups/433296140360035/</a><br />
+Unserer geschlossenen WhatsApp-Gruppe kann ich dich hinzufügen, wenn du mir dafür auf WhatsApp eine kurze Nachricht schreibst &ndash; mit <a href="https://api.whatsapp.com/send?phone=4917657517450&text=Hallo+Marvin%2C+bitte+f%C3%BCge+mich+der+LiGa-WhatsApp-Gruppe+hinzu%21+{url_vorname}+{url_name}+%28Mitgliedsnummer+{user_id}%29%2E%20Ich+bin+damit+einverstanden%2C+dass+mit+Hinzuf%C3%BCgen+in+diese+Gruppe+meine+Handynummer+an+WhatsApp+und+alle+Mitglieder+in+der+Gruppe+weitergegeben+wird%2E">diesem Link</a> geht das ganz einfach. Dazu ein notwendiger Hinweis zum Datenschutz: Wenn du uns mitteilst, dass du zu unserer LiGa-WhatsApp-Gruppe hinzugefügt werden möchtest, erklärst du dich damit einverstanden, dass mit Hinzufügen in diese Gruppe deine Handynummer an WhatsApp und alle Mitglieder in der Gruppe weitergegeben wird.<br />
+<br />
+Noch ein kurzer Hinweis zu der WhatsApp-Gruppe: Diese ist ausschließlich für Vereins- und Gaming-Themen gedacht. Du findest in der Gruppenbeschreibung aber auch einen Link zur LiGa-Talk-Gruppe, in der über alles Mögliche diskutiert werden kann. Auch haben wir weitere Gruppen speziell für einzelne Spiele eingerichtet. Eine Liste mit Einladungslinks dazu findest du ebenfalls in der Gruppenbeschreibung.<br />
+<br />
+Falls du irgendwelche Fragen hast, kannst du dich jederzeit gerne an mich wenden.<br />
+<br />
+Liebe Grüße<br />
+<br />
+Marvin Ruder<br />
+<br />
+<br />
+————————————————————————————————<br />
+<br />
+Liberale Gamer e.V.<br />
+<br />
+Marvin Ruder<br />
+Mitgliederbetreuung<br />
+<br />
+Tel.:   06224 9266995<br />
+Fax:    06224 9282579<br />
+Mobil:  0176 57517450<br />
+<br />
+<a href="mailto:marvin.ruder@liberale-gamer.gg">marvin.ruder@liberale-gamer.gg</a><br />
+<a href="https://www.liberale-gamer.gg">www.liberale-gamer.gg</a><br />"""
 
     url_receiver = urllib.parse.quote_plus(receiver)
     url_subject = urllib.parse.quote_plus(subject)
@@ -321,7 +324,7 @@ def send_mail(user_id):
     
     if request.method == 'POST':
         if "send" in request.form:
-            send_mail.send_email('Marvin Ruder <mitgliedsantrag@liberale-gamer.gg>', receiver, 'Marvin Ruder <marvin.ruder@liberale-gamer.gg>', subject, text)
+            sendmail.send_email('Marvin Ruder <mitgliedsantrag@liberale-gamer.gg>', receiver, 'Marvin Ruder <marvin.ruder@liberale-gamer.gg>', subject, text)
             flash('Mail versendet')
             return redirect(url_for('edit',user_id=str(user_id)))
         return redirect(url_for('edit',user_id=str(user_id)))
