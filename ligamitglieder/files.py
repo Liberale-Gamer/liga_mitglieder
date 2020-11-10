08bcc5a836f6ea7c9170562d05d86013b94ec836
Marvin A. Ruder <ruder.marvin@icloud.com>
2020-11-10 16:43:37 +0100

    Change gitignore

--- ligamitglieder/files.py
+++
@@ -1,6 +0,0 @@
-foldernames = {}
-#'protokolle': 'Protokolle'} # Example folder
-
-filenames = {}
-#'doc': 'Dokument',\ # Example file in root
-#'protokolle/protokoll': 'Protokoll'} # Example file in folder
